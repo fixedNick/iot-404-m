93dbbbde1cb5f2101cd2f5edc74d57aa6c6b82ee
fixedNick <kkos44911@gmail.com>
2026-04-28 16:29:26 +0300

    splitted subscribes, now allowed to add subsriptions manually

--- server/internal/mqtt/subscribes.go
+++ server/internal/mqtt/subscribes.go
@@ -7,26 +7,37 @@ import (
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
-func (m *MQTTClient) SetupSubsribes() {
+func (m *MQTTClient) SetupSubsribes(subsribes ...func(*MQTTClient)) {
 	go func() {
-		m.Run()
-		go m.Subscribe("sensors/data/wind", QoS_HIGH, func(client mqtt.Client, msg mqtt.Message) {
-			w := Wind{}
-			json.Unmarshal(msg.Payload(), &w)
-			fmt.Printf("Callback: sensors/data/wind. Recv: %v.\n", w)
-			m.wind <- w
-		})
-		go m.Subscribe("sensors/data/temperature", QoS_HIGH, func(client mqtt.Client, msg mqtt.Message) {
-			t := Temperature{}
-			json.Unmarshal(msg.Payload(), &t)
-			fmt.Printf("Callback: sensors/data/wind. Recv: %v.\n", t)
-			m.temp <- t
-		})
-		go m.Subscribe("sensors/data/humidity", QoS_HIGH, func(client mqtt.Client, msg mqtt.Message) {
-			h := Humidity{}
-			json.Unmarshal(msg.Payload(), &h)
-			fmt.Printf("Callback: sensors/data/wind. Recv: %v.\n", h)
-			m.humidity <- h
-		})
+		for _, sub := range subsribes {
+			go sub(m)
+		}
 	}()
+}
+
+func SuscribeWindSpeed(m *MQTTClient) {
+	m.Subscribe("sensors/data/wind", QoS_HIGH, func(client mqtt.Client, msg mqtt.Message) {
+		w := Wind{}
+		json.Unmarshal(msg.Payload(), &w)
+		fmt.Printf("Callback: sensors/data/wind. Recv: %v.\n", w)
+		m.wind <- w
+	})
+}
+
+func SuscribeTemperature(m *MQTTClient) {
+	m.Subscribe("sensors/data/temperature", QoS_HIGH, func(client mqtt.Client, msg mqtt.Message) {
+		t := Temperature{}
+		json.Unmarshal(msg.Payload(), &t)
+		fmt.Printf("Callback: sensors/data/wind. Recv: %v.\n", t)
+		m.temp <- t
+	})
+}
+
+func SuscribeHumidity(m *MQTTClient) {
+	m.Subscribe("sensors/data/humidity", QoS_HIGH, func(client mqtt.Client, msg mqtt.Message) {
+		h := Humidity{}
+		json.Unmarshal(msg.Payload(), &h)
+		fmt.Printf("Callback: sensors/data/wind. Recv: %v.\n", h)
+		m.humidity <- h
+	})
 }
