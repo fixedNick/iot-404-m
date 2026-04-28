35286dde4cdadda1c22c6a2527672265063e0ea9
fixedNick <kkos44911@gmail.com>
2026-04-28 12:52:23 +0300

    config load rewrite on ENV`s

--- server/internal/config/config.go
+++ server/internal/config/config.go
@@ -7,8 +7,24 @@ import (
 )
 
 type Config struct {
-	GRPC  GRPCConfig  `yaml:"Local"`
-	MySQL MySQLConfig `yaml:"MySQL"`
+	GRPC       GRPCConfig       `yaml:"Local"`
+	MySQL      MySQLConfig      `yaml:"MySQL"`
+	MQTT       MQTTConfig       `yaml:"MQTT"`
+	Repository RepositoryCofnig `yaml:"Repository"`
+}
+
+type GRPCConfig struct {
+	Port int `yaml:"Port"`
+	// request - response timeout in milliseconds
+	Timeout uint `yaml:"Timeout"`
+}
+type RepositoryCofnig struct {
+	CacheSize uint `yaml:"CacheLimit"`
+}
+
+type MQTTConfig struct {
+	Host string `yaml:"Host"`
+	Port int    `yaml:"Port"`
 }
 
 type MySQLConfig struct {
@@ -23,17 +39,24 @@ func (m MySQLConfig) DSN() string {
 	return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", m.User, m.Password, m.Host, m.Port, m.Database)
 }
 
-type GRPCConfig struct {
-	Port int `yaml:"Port"`
-}
-
-func MustLoadConfig(path string) *Config {
+func MustLoadConfig() *Config {
 	cfg := Config{}
 
 	var err error
+	// grpc
+
 	if cfg.GRPC.Port, err = strconv.Atoi(os.Getenv("GRPC_PORT")); err != nil {
 		panic(fmt.Sprintf("Failed to parse env GRPC_PORT: %v", err))
 	}
+
+	timeout, err := strconv.ParseUint(os.Getenv("GRPC_TIMEOUT"), 10, 32)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to parse env GRPC_TIMEOUT: %v", err))
+	}
+
+	cfg.GRPC.Timeout = uint(timeout)
+
+	// sql
 
 	cfg.MySQL.Database = os.Getenv("MYSQL_DATABASE")
 	if cfg.MySQL.Database == "" {
@@ -54,6 +77,31 @@ func MustLoadConfig(path string) *Config {
 	if cfg.MySQL.Password == "" {
 		panic("Required env MYSQL_PASSWORD is not set")
 	}
+
+	// mqtt
+
+	cfg.MQTT.Host = os.Getenv("MQTT_HOST")
+	if cfg.MQTT.Host == "" {
+		panic("Required env MQTT_HOST is not set")
+	}
+
+	cfg.MQTT.Port, err = strconv.Atoi(os.Getenv("MQTT_PORT"))
+	if err != nil || cfg.MQTT.Port == 0 {
+		panic(fmt.Sprintf("Failed to parse env MQTT_PORT: %v", err))
+	}
+
+	// cache
+
+	cs, err := strconv.Atoi(os.Getenv("CACHE_SIZE"))
+	if err != nil {
+		panic(fmt.Sprintf("Failed to parse env CACHE_SIZE: %v", err))
+	}
+
+	if cs <= 0 {
+		panic("Required env CACHE_SIZE is not set. Must be greater than 0")
+	}
+
+	cfg.Repository.CacheSize = uint(cs)
 
 	return &cfg
 }
