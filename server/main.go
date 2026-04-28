dd7f934e7b800d67067714d328166600d6ab7687
fixedNick <kkos44911@gmail.com>
2026-04-28 12:53:23 +0300

    entrypoint updated

--- server/main.go
+++ server/main.go
@@ -1,63 +1,38 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 	"os/signal"
-	"server/domain"
+	cfg "server/internal/config"
+	"server/internal/grpc"
+	"server/internal/mqtt"
+	serverhead "server/internal/server_head"
+	db "server/internal/storage/service"
 	"syscall"
 	"time"
-
-	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
 func main() {
-	storage := domain.NewStorage()
+	config := cfg.MustLoadConfig()
 
-	mqttClient := domain.NewMQTTClient("176.109.106.237", 1883, fmt.Sprintf("go-%d", time.Now().Unix()), nil)
-	defer mqttClient.Disconnect()
+	mqttClient := mqtt.NewMQTTClient(config.MQTT.Host, config.MQTT.Port, fmt.Sprintf("go-%d", time.Now().Unix()), nil)
+	storage := db.New(config)
+	serverHead := serverhead.New(mqttClient, storage)
 
-	gs := domain.NewGRPCServer(
-		domain.MustLoadConfig("config/cfg.yaml"),
-		storage,
-		mqttClient,
-	)
+	gs := grpc.NewGRPCServer(config, serverHead)
+	mqttClient.SetupSubsribes()
 
-	go func() {
-		mqttClient.Run()
-		go mqttClient.Subscribe("sensors/data/wind", domain.QoS_HIGH, func(client mqtt.Client, msg mqtt.Message) {
-			type Wind struct {
-				Voltage float64 `json:"voltage"`
-				Speed   float64 `json:"speed"`
-			}
-
-			w := Wind{}
-			json.Unmarshal(msg.Payload(), &w)
-			err := storage.Save(domain.NewIndication(w.Voltage, w.Speed, time.Now()))
-			if err != nil {
-				fmt.Printf("[error] Save err: %v. Received: %v", err, w)
-				return
-			}
-			fmt.Printf("Saved %v.\n-----------------\n", w)
-			mqttClient.Sync <- struct{}{}
-		})
-		go mqttClient.Subscribe("sensors/data/temperature", domain.QoS_HIGH, func(client mqtt.Client, msg mqtt.Message) {
-			msg.Payload()
-		})
-		go mqttClient.Subscribe("sensors/data/humidity", domain.QoS_HIGH, func(client mqtt.Client, msg mqtt.Message) {
-			msg.Payload()
-		})
-	}()
 	go gs.Run()
 
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
 	<-stop
 
-	close(mqttClient.Sync)
-
 	gs.Stop()
+	mqttClient.Close()
+	storage.Close()
+
 	log.Println("Server successfully stopped")
 }
