24463bf295d62b4ec9af6331cda8034926fd7946
fixedNick <kkos44911@gmail.com>
2026-05-10 22:44:46 +0300

    upds

--- mysql/init.sql
+++ mysql/init.sql
@@ -16,3 +16,15 @@ CREATE TABLE IF NOT EXISTS humidity (
     val FLOAT NOT NULL,
     ts BIGINT UNSIGNED NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS auto_collect_status (
+    sensor_id INT NOT NULL,
+    sensor_name VARCHAR(128) NOT NULL,
+    sensor_status TINYINT(1) NOT NULL,
+    UNIQUE (sensor_id)
+);
+
+INSERT INTO auto_collect_status (sensor_id, sensor_name, sensor_status) VALUES 
+(1, 'temperature', 0),
+(2, 'wind_speed', 0),
+(3, 'humidity', 0);
